7e21b47e80b32f8bd8d29f42a145e591eee2dc35
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-25 15:40:52 +0900

    after test setup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 import { app } from '../app'
 
 let mongo: any
