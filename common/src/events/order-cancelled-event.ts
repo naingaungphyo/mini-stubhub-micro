0c766f5131cdae506aa72d4a5651f29c91276896
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-09-01 00:02:46 +0900

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from './subjects'
 
 export interface OrderCancelledEvent {
-  subject: Subjects.OrderCreated
+  subject: Subjects.OrderCancelled
   data: {
     id: string
     ticket: {
