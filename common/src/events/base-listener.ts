cd709f083468d86859a0c202787551aafe5fd343
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-09-05 22:28:06 +0900

    Updates

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -1,20 +1,20 @@
-import { Message, Stan } from 'node-nats-streaming';
-import { Subjects } from './subjects';
+import { Message, Stan } from 'node-nats-streaming'
+import { Subjects } from './subjects'
 
 interface Event {
-  subject: Subjects;
-  data: any;
+  subject: Subjects
+  data: any
 }
 
 export abstract class Listener<T extends Event> {
-  abstract subject: T['subject'];
-  abstract queueGroupName: string;
-  abstract onMessage(data: T['data'], msg: Message): void;
-  private client: Stan;
-  protected ackWait = 5 * 1000;
+  abstract subject: T['subject']
+  abstract queueGroupName: string
+  abstract onMessage(data: T['data'], msg: Message): void
+  protected client: Stan
+  protected ackWait = 5 * 1000
 
   constructor(client: Stan) {
-    this.client = client;
+    this.client = client
   }
 
   subscriptionOptions() {
@@ -23,28 +23,22 @@ export abstract class Listener<T extends Event> {
       .setDeliverAllAvailable()
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
+      .setDurableName(this.queueGroupName)
   }
 
   listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
+    const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions())
 
     subscription.on('message', (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`)
 
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
+      const parsedData = this.parseMessage(msg)
+      this.onMessage(parsedData, msg)
+    })
   }
 
   parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === 'string'
-      ? JSON.parse(data)
-      : JSON.parse(data.toString('utf8'));
+    const data = msg.getData()
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'))
   }
 }
